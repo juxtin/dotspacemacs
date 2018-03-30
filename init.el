6dde94b732b2885f26a9b150623f2da29d6ebede
Justin Holguin <justin.holguin@healthsparq.com>
2018-03-30 14:38:31 -0700

    Italicize docstrings

--- init.el
+++ init.el
@@ -459,5 +459,6 @@ This function is called at the very end of Spacemacs initialization."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((((class color) (min-colors 257)) (:foreground "#F8F8F2" :background "#272822")) (((class color) (min-colors 89)) (:foreground "#F5F5F5" :background "#1B1E1C")))))
+ '(default ((((class color) (min-colors 257)) (:foreground "#F8F8F2" :background "#272822")) (((class color) (min-colors 89)) (:foreground "#F5F5F5" :background "#1B1E1C"))))
+ '(font-lock-doc-face ((t (:foreground "light goldenrod" :slant italic)))))
 )
